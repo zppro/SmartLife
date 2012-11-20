40318da99a4e20731776c7808288f3880ad24ac2
zppro <zpprozppro@gmail.com>
2012-11-20 13:54:20 +0800

    Initial Commit

---
+++ SmartLife/ZPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZPAppDelegate.h
+//  SmartLife
+//
+//  Created by zppro on 12-11-20.
+//  Copyright (c) 2012年 zppro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartLife/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmartLife
+//
+//  Created by zppro on 12-11-20.
+//  Copyright (c) 2012年 zppro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZPAppDelegate class]));
+    }
+}
